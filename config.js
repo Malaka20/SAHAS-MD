e8ed195570bb40c8955bcd4663e99a3c013d3ba1
Malaka20/NETHU-MD-V1 2 <153556272+Malaka20@users.noreply.github.com>
2024-11-15 13:00:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "Put Session Id",
+  SESSION_ID: process.env.SESSION_ID || "PKZwQbpT#MVhkPhmJynVt_RinqpF15XY_UwSxBfDFiB_uRK5CudM",
   // ADD Your Session Id 
-  MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:QNhcGLhogVTTeoAqudBYkWfchsXAzyPO@autorack.proxy.rlwy.net:26777",
     // ADD Your MongoDB Database URL
   PREFIX: process.env.PREFIX || ".",
   // Add Your Custom Prefix 
